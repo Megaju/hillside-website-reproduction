883bfe66622db056be6e889338cb4b7f83bfad4c
Megaju <julien_malle@outlook.fr>
2016-11-08 12:19:09 +0100

    modif href - seulement 2 etats pour le test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 
 $( document ).ready(function() {
+    // menu déroulant top
     $(".nav-left ul li a").hover(function(){
         $(".menu-plus").css("top", "80px"); 
     });
     
     $("header").mouseleave(function(){
         $(".menu-plus").css("top", "0"); 
+    });
+    
+    // scroll fluid des sections principales et modif des href
+    var currentSection = 0;
+    
+    $("#ancre").click(function(){
+        if (currentSection === 0) {
+            currentSection++;
+        } else {
+            $(this).attr("href", "#ancre2");
+        }
+    });
+    
+    $('a[href^="#"]').click(function(){
+        var the_id = $(this).attr("href");
+        
+        $('html, body').animate({
+            scrollTop:$(the_id).offset().top
+        }, 'slow');
+        return false;
     });
 });
